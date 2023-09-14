11a6155c7b6919cad6286b433fed69ed7aa6708a
Igor Herson <igorhaf@gmail.com>
2023-09-14 17:24:39 -0300

    axios reading operations successful, some layout fixes

--- src/components/layout/WebLayout.js
+++ src/components/layout/WebLayout.js
@@ -1,30 +1,66 @@
 import "./weblayout.css";
 import {Component} from "react";
-import {Button} from "@blueprintjs/core";
+import axiosInstance from '../../api/axiosInstance';
+import {Button, Menu, MenuDivider, MenuItem} from "@blueprintjs/core";
 import { Colors } from "@blueprintjs/core";
 
+
 class WebLayout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            operations: [],
+            loading: true,
+            error: null
+        };
+    }
+    async componentDidMount() {
+        try {
+            const response = await axiosInstance.get('/operations');  // TODO: substitua pelo endpoint correto
+            this.setState({
+                operations: response.data,
+                loading: false
+            });
+        } catch (error) {
+            this.setState({
+                error: error.message,
+                loading: false
+            });
+        }
+    }
     render() {
+        const { loading, error, operations } = this.state;
         return (
             <div>
                 <body>
-                <header class="page-header" style={{ color: Colors.LIGHT_GRAY1, background: Colors.DARK_GRAY4 }}>
+                <header className="page-header" style={{ color: Colors.LIGHT_GRAY1, background: Colors.DARK_GRAY4 }}>
                     <h1>Header</h1>
                 </header>
 
-                <main class="page-content">
-                    <nav class="content-navigation" style={{ color: Colors.LIGHT_GRAY1, background: Colors.DARK_GRAY5 }}>
-                        <h1>Navigation</h1>
+                <main className="page-content">
+                    <nav className="content-navigation" style={{ color: Colors.LIGHT_GRAY1, background: Colors.DARK_GRAY5 }}>
+                        <Menu roleStructure="listoption" class="content-navigation" style={{ color: Colors.LIGHT_GRAY1, background: Colors.DARK_GRAY5, minWidth:0 }} small={true}>
+                            {loading && <p>Carregando...</p>}
+                            {error && <p>Erro: {error}</p>}
+                            {operations.map(operations => (
+                                <MenuItem icon={operations.icon} text={operations.name} />
+                            ))}
+                           {/* <MenuDivider />
+                            <MenuItem text="Settings..." icon="cog" intent="primary">
+                                <MenuItem icon="tick" text="Save on edit" />
+                                <MenuItem icon="blank" text="Compile on edit" />
+                            </MenuItem>*/}
+                        </Menu>
                     </nav>
-                    <article class="content-article" style={{ color: Colors.LIGHT_GRAY1, background: Colors.DARK_GRAY2 }}>
+                    <article className="content-article" style={{ color: Colors.LIGHT_GRAY1, background: Colors.DARK_GRAY2 }}>
                         <h1><Button intent="success" text="Clique em mim!" /></h1>
                     </article>
-                    <aside class="content-sidebar" style={{ color: Colors.LIGHT_GRAY1, background: Colors.DARK_GRAY5 }}>
+                    <aside className="content-sidebar" style={{ color: Colors.LIGHT_GRAY1, background: Colors.DARK_GRAY5 }}>
                         <h1>Sidebar</h1>
                     </aside>
                 </main>
 
-                <footer class="page-footer">
+                <footer className="page-footer">
                     <h1>Footer</h1>
                 </footer>
                 </body>
