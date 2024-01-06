e33db05194613005f9ab5f02de260766cf67ff4d
Igor Herson <igorhaf@gmail.com>
2024-01-06 01:29:42 -0300

    basic structure

---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="justify-center flex bg-black items-center h-screen">
+        <div class="text-6xl">
+            Hello 👋🏼
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+};
+</script>
