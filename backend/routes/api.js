c7a0e4930c920ccdb30db8f5986d7e13861ca38b
Igor Herson <igorhaf@gmail.com>
2024-01-12 18:58:45 -0300

    change frontend name application

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const graphController = require('../controllers/graphController');
+const {sendCommand} = require("../controllers/components/terminalController");
 
 router.post('/graph-data', graphController.createGraph);
 router.get('/get-latest-diagram', graphController.getGraph);
+router.get('/execute-command', sendCommand);
 
 module.exports = router;
