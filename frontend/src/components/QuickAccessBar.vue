da45016c204d3769fb09fc91b5113b71e1a26328
Igor Herson <igorhaf@gmail.com>
2024-01-12 10:42:48 -0300

    change some quick menu css, fixeds need

--- frontend/src/components/QuickAccessBar.vue
+++ frontend/src/components/QuickAccessBar.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="quick-access-bar w-1/6">
+      <div @click="showComponent('treeView')" class="qm-icons play"></div>
     <div @click="showComponent('treeView')" class="qm-icons components"></div>
     <div @click="showComponent('project')" class="qm-icons frames"></div>
+      <div @click="showComponent('treeView')" class="qm-icons database"></div>
     <div @click="showComponent('project')" class="qm-icons integrations"></div>
-    <div @click="showComponent('treeView')" class="qm-icons play"></div>
-    <div @click="showComponent('treeView')" class="qm-icons database"></div>
+
+
     <div @click="showComponent('project')" class="qm-icons config"></div>
   </div>
 </template>
