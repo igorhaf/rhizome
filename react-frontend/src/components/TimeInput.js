e600b8979bd8c3d9622a8d3990b95ef00fce74b1
Igor Herson <igorhaf@gmail.com>
2024-04-11 12:57:20 -0300

    ajusts and add objects forms

--- react-frontend/src/components/TimeInput.js
+++ react-frontend/src/components/TimeInput.js
@@ -8,7 +8,7 @@ const TimeInput = ({ label, value, onChange }) => {
                 type="time"
                 value={value}
                 onChange={onChange}
-                className="focus:outline-none focus:box-shadow-blue-500 shadow text-primary appearance-none w-full py-1 px-1 bg text border focus:border-blue-500 focus:outline-blue leading-tight"
+                className="focus:outline-none focus:box-shadow-blue-500 shadow text-primary appearance-none w-full py-1 px-1 bg text border focus:border-blue-500 focus:outline-blue leading-tight h-6"
             />
         </div>
     );
