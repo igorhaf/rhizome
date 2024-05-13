bbfbf8fae3f54baf24222b2653c59afc8783d56c
Igor Herson <igorhaf@gmail.com>
2024-05-13 14:39:07 -0300

    remove javascript files, keep typescript new version

--- backend/models/Tab.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const tabSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    position: { type: Number, required: true, default: 0 },
-    status: { type: String, default: 'open' }  // Assuming status can be 'open', 'closed', etc.
-});
-
-module.exports = mongoose.model('Tab', tabSchema);
