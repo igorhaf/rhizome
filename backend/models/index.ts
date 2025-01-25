5ba526acfa6fd8f9ffe0f5081568a4b9f4c4f389
Igor Herson <igorhaf@gmail.com>
2025-01-25 10:07:55 -0300

    refacting-prototype-6

--- backend/models/index.ts
+++
@@ -1,6 +0,0 @@
-import { Sequelize } from 'sequelize';
-import config from '../config/config.json'; // Ajuste o caminho conforme necessário
-
-const sequelize = new Sequelize(config.development);
-
-export default sequelize;
