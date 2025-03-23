e44904ac84e46868294578e427b2f9defb10f39c
Igor Herson <igorhaf@gmail.com>
2025-03-23 03:00:57 -0300

    purge all code

--- frontend/src/redux/actions/actionTypes.js
+++
@@ -1,6 +0,0 @@
-export const ADD_FRAGMENT = 'ADD_FRAGMENT';
-export const REMOVE_FRAGMENT = 'REMOVE_FRAGMENT';
-
-// Form actions
-export const ADD_FORM = 'ADD_FORM';
-export const REMOVE_FORM = 'REMOVE_FORM';
