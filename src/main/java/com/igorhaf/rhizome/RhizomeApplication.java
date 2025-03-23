3caad8b311ed7e29a3b7f8536e2bc1e9e13f807f
Igor Herson <igorhaf@gmail.com>
2025-03-23 03:15:18 -0300

    start new backend api spring boot

---
+++ src/main/java/com/igorhaf/rhizome/RhizomeApplication.java
@@ -0,0 +1,13 @@
+package com.igorhaf.rhizome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhizomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhizomeApplication.class, args);
+	}
+
+}
